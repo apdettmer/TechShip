510ffb467b9bad513211f99cb6310b20531bcd7d
Adam Wang <adw74@cornell.edu>
2019-11-03 18:49:58 -0500

    Nothing

---
+++ project/_build/main.ml
@@ -0,0 +1,12 @@
+(** [main ()] prompts for the game to play, then starts it. *)
+let main () =
+  ANSITerminal.(print_string [red]
+                  "\n\nWelcome to the 3110 Text Adventure Game engine.\n");
+  print_endline "Please enter the name of the game file you want to load.\n";
+  print_string  "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | file_name -> ()
+
+(* Execute the game engine. *)
+let () = main ()
