2a498014936570b0f29e25e764e32bbb7f105aaa
Eric Whitehead <ew424@cornell.edu>
2019-11-03 21:34:30 -0500

    Update founding.mli

--- project/founding.mli
+++ project/founding.mli
@@ -1,1 +1,30 @@
 (* design the company in the growth phase *)
+type company
+
+type employee
+
+type product
+
+type investor
+
+(**  *)
+val product : company -> product
+
+(**  *)
+val funding : company -> int
+
+(**  *)
+val employees : company -> employee list
+
+(**  *)
+val reputation : company -> int
+
+(**  *)
+val morale : company -> int
+
+(**  *)
+val investors : company -> investor list
+
+val print_stats: company -> unit
+
+
