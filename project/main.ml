fbc52417ce076a08bb2ae77c2ab42678bb61fd31
Eric Whitehead <ew424@cornell.edu>
2019-11-03 21:36:34 -0500

    Update main.ml

--- project/main.ml
+++ project/main.ml
@@ -1,3 +1,12 @@
+exception Empty
+
+(**[play] is the repl loop that takes player input and determines actions
+   in the game. [player_file] is a JSON file that is a save file. *)
+let play player_file = 
+  failwith "."
+
+
+
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.(print_string [red]
@@ -5,8 +14,6 @@ let main () =
   print_endline "Would you like to start a new game or load a save?\n";
   print_string  "> ";
   match read_line () with
-  | exception End_of_file -> ()
-  | file_name -> ()
-
-(* Execute the game engine. *)
-let () = main ()
+  | exception Empty -> ()
+  | "1" -> play 
+  | _ -> play
