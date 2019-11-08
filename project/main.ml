9b66ab8b03a6cb4f29c0426f0862e0febccbe820
Adam Wang <adw74@cornell.edu>
2019-11-08 17:38:35 -0500

    Implemented ability to delete save files

--- project/main.ml
+++ project/main.ml
@@ -9,10 +9,7 @@ open Event
 let rec play company =
   display_status company
 
-let load_file file_name =
-  Yojson.Basic.from_file file_name |> load |> play
-
-let create_new_game () =
+let create_new_save () =
   ANSITerminal.(print_string [green] ">be you\n");
   ANSITerminal.(print_string [green] ">recent graduate of Cornell Engineering\n");
   ANSITerminal.(print_string [green] ">broke and jobless\n");
@@ -31,35 +28,44 @@ let json_extension file =
     true
   with Not_found -> false
 
+type load_or_delete = Load | Delete
+
+let save_file_helper load_or_delete file_name =
+  match load_or_delete with
+  | Load -> Yojson.Basic.from_file file_name |> load |> play
+  | Delete -> Sys.remove file_name
+
 let list_files () =
   Sys.readdir "." |> Array.to_list |> List.filter (fun x -> json_extension x) |> List.map (fun x -> Str.global_replace (Str.regexp_string ".json") "" x) |> List.sort String.compare
 
-let rec find_file save_files input =
+let rec find_file load_or_delete save_files input=
   if List.mem input save_files
-  then String.concat "" [input; ".json"] |> load_file
+  then String.concat "" [input; ".json"] |> save_file_helper load_or_delete
   else (print_endline "Invalid entry.";
         print_endline "";
-        load_save_file ())
+        save_file load_or_delete)
 
 and
 
-  load_save_file () =
-  print_endline "Select a game:";
+  save_file load_or_delete =
+  print_endline "Select a save:";
   let save_files = list_files () in
   List.iter print_endline save_files;
   print_string ">";
-  read_line () |> find_file save_files
+  read_line () |> find_file load_or_delete save_files
 
 let rec main_menu_helper () =
   print_endline "Invalid entry.";
   print_endline "";
-  print_endline "[1] Create new game.";
-  print_endline "[2] Load game.";
+  print_endline "[0] Create new save.";
+  print_endline "[1] Load save.";
+  print_endline "[2] Delete save.";
   print_endline "[3] Quit.";
   print_string ">";
   match read_line () with
-  | "1" -> print_endline ""; create_new_game ()
-  | "2" -> print_endline ""; load_save_file ()
+  | "0" -> print_endline ""; create_new_save ()
+  | "1" -> print_endline ""; save_file Load
+  | "2" -> print_endline ""; save_file Delete
   | "3" -> print_endline ""; exit 0
   | _ -> main_menu_helper ()
 
@@ -69,13 +75,15 @@ let main_menu () =
   print_endline "";
   print_endline "T E C H S H I P";
   print_endline "";
-  print_endline "[1] Create new game.";
-  print_endline "[2] Load game.";
+  print_endline "[0] Create new save.";
+  print_endline "[1] Load save.";
+  print_endline "[2] Delete save.";
   print_endline "[3] Quit.";
   print_string ">";
   match read_line () with
-  | "1" -> print_endline ""; create_new_game ()
-  | "2" -> print_endline ""; load_save_file ()
+  | "0" -> print_endline ""; create_new_save ()
+  | "1" -> print_endline ""; save_file Load
+  | "2" -> print_endline ""; save_file Delete
   | "3" -> print_endline ""; exit 0
   | _ -> main_menu_helper ()
 
