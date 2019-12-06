36baccbdfcc907c00d66ee8e7c967b4afce88f90
Andrew Dettmer <apd67@cornell.edu>
2019-12-05 21:38:31 -0500

    A start on the second phase?  Feel free to change these

--- project/growth.ml
+++ project/growth.ml
@@ -1,1 +1,21 @@
-(* TODO: Create a module that models the company in the startup phase *)
+open Unix
+
+type founded = {
+  product : Founding.product;
+  funding : int;
+  reputation : int;
+  morale : int;
+  teams : Founding.employee list list;
+  investors: Founding.investor list;
+  date : tm
+}
+
+let found company = 
+  { product = Founding.product company;
+    funding = Founding.funding company;
+    reputation = Founding.reputation company;
+    morale = Founding.morale company;
+    teams = [];
+    investors = Founding.investors company;
+    date = Founding.date company
+  }
--- project/growth.mli
+++ project/growth.mli
@@ -1,1 +1,6 @@
-(* Define the company in the startup phase *)
+(* the type representing the founded company *)
+type founded
+
+(** [found company] gives a new founded company using stats from
+    [company] *)
+val found : Founding.company -> founded
