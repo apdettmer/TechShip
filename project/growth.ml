94275090fa710ceca2344a73282b1f05a1ce4ab3
Eric Whitehead <ew424@cornell.edu>
2019-12-08 15:49:26 -0500

    Started working on growth.ml and growth.mli

--- project/growth.ml
+++ project/growth.ml
@@ -2,20 +2,168 @@ open Unix
 
 type founded = {
   product : Founding.product;
-  funding : int;
+  market_cap : int;
   reputation : int;
   morale : int;
   teams : Founding.employee list list;
   investors: Founding.investor list;
-  date : tm
+  date : tm;
+  marketing: int;
+  management: int
+
 }
 
 let found company = 
-  { product = Founding.product company;
-    funding = Founding.funding company;
+  { 
+    product = Founding.product company;
+    market_cap = Founding.funding company;
     reputation = Founding.reputation company;
     morale = Founding.morale company;
     teams = [];
     investors = Founding.investors company;
-    date = Founding.date company
+    date = Founding.date company;
+    marketing = 100;
+    management = 50
   }
+
+
+(* Currently defined exactly as the one in event - but I feel like its better to
+   create a new type for this file / new type of company -ew424 *)
+type f_response = { 
+  description : string;
+  effects : (string * int option) list
+}
+
+let found company = 
+  { 
+    product = Founding.product company;
+    market_cap = Founding.funding company;
+    reputation = Founding.reputation company;
+    morale = Founding.morale company;
+    teams = [];
+    investors = Founding.investors company;
+    date = Founding.date company;
+    marketing = 50;
+    management = 100;
+  }
+
+let product founded = 
+  founded.product
+
+let market_cap founded = 
+  founded.market_cap
+
+let reputation founded = 
+  founded.reputation
+
+let morale founded =
+  founded.morale
+
+let teams founded = 
+  founded.teams
+
+let investors founded =
+  founded.investors
+
+let date founded =
+  founded.date
+
+let marketing founded = 
+  founded.marketing
+
+let management founded = 
+  founded.management
+
+
+let update_investors current_investor_list = 
+  failwith "Not sure how we wanna go about implementing this"
+
+let update_teams current_team_list =
+  failwith "Again, not sure how we should do this"
+
+let update_cat name v company= 
+  match name with 
+  | "market_cap" -> { 
+      product = product company;
+      market_cap = market_cap company + v;
+      reputation = reputation company;
+      morale = morale company;
+      teams = teams company;
+      investors = investors company;
+      date = date company;
+      marketing = marketing company;
+      management = management company;
+    }
+  | "reputation" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company + v;
+      morale = morale company;
+      teams = teams company;
+      investors = investors company;
+      date = date company;
+      marketing = marketing company;
+      management = management company;
+    }
+  | "morale" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company;
+      morale = morale company + v;
+      teams = teams company;
+      investors = investors company;
+      date = date company;
+      marketing = marketing company;
+      management = management company;
+    }
+  | "investors" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company;
+      morale = morale company;
+      teams = teams company;
+      investors = update_investors (investors company);
+      date = date company;
+      marketing = marketing company;
+      management = management company;
+    }
+  | "marketing" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company;
+      morale = morale company;
+      teams = teams company;
+      investors = investors company;
+      date = date company;
+      marketing = marketing company + v;
+      management = management company;
+    }
+  | "management" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company;
+      morale = morale company;
+      teams = teams company;
+      investors = investors company;
+      date = date company;
+      marketing = marketing company;
+      management = management company + v;
+    }
+  | "teams" -> { 
+      product = product company;
+      market_cap = market_cap company;
+      reputation = reputation company;
+      morale = morale company;
+      teams = update_teams (teams company);
+      investors = investors company;
+      date = date company;
+      marketing = marketing company;
+      management = management company;
+    }
+  | _ -> failwith "ERROROR"
+
+let rec update_founded founded f_resp = 
+  match f_resp with 
+  | [] -> founded
+  | (s, Some(v)) :: t -> update_founded (update_cat s v founded) t
+  | _ -> failwith "ERROROR"
--- project/growth.mli
+++ project/growth.mli
@@ -4,3 +4,41 @@ type founded
 (** [found company] gives a new founded company using stats from
     [company] *)
 val found : Founding.company -> founded
+
+(** the type representing a response to an event from this phase *)
+type f_response
+
+(** [found company] gives a new founded company using stats from
+    [company] *)
+val found : Founding.company -> founded
+
+(**[product founded] is the product field of company [founded]*)
+val product : founded -> Founding.product
+
+(**[market_cap founded] is the market_cap field of company [founded]*)
+val market_cap :  founded -> int 
+
+(**[reputation founded] is the reputation field of company [founded]*)
+val reputation : founded -> int
+
+(**[morale founded] is the morale field of company [founded]*)
+val morale: founded -> int
+
+(**[teams founded] is the teams field of company [founded]*)
+val teams : founded -> Founding.employee list list 
+
+(**[investors founded] is the investors field of company [founded]*)
+val investors : founded -> Founding.investor list
+
+(**[date founded] is the date field of company [founded]*)
+val date : founded -> Unix.tm
+
+(**[marketing founded] is the marketing field of company [founded]*)
+val marketing : founded -> int 
+
+(**[management founded] is the management field of company [founded]*)
+val management : founded -> int
+
+(* (**[update_founded founded f_resp] takes a [founded] company and applies the 
+   stat changes provided in [f_resp] and returns a new, updated company *)
+   val update_founded : founded -> f_response -> founded *)
